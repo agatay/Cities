e851aeb6c03f12cb8a7fc14fdcd672de0737883a
Emre Can Agatay <EmreCan.Agatay@yapikredi.com.tr>
2016-08-26 11:57:17 +0300

    2

--- app/src/main/java/com/example/mac/city/MainActivity.java
+++ app/src/main/java/com/example/mac/city/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.mac.city;
 
-import android.content.Intent;
+
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -20,28 +24,41 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        mEdit = (EditText) findViewById(R.id.editText);
+
+        Log.w("activity","oncreate1");
+
+        Fragment fr = new MainActivityFragment();
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_place, fr, "zaaa");
+        fragmentTransaction.commit();
+
+        Log.w("activity","oncreate2");
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                mEdit = (EditText) findViewById(R.id.editText);
 
-                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                //Intent i = new Intent(getApplicationContext(), MainActivity.class);
                 if (mEdit.getText().length() > 0) {
-                    i.putExtra("key", mEdit.getText().toString());
-                    startActivity(i);
+                    //i.putExtra("key", mEdit.getText().toString());
+                    //startActivity(i);
+                    Log.w("activity","oncreate3");
+                    MainActivityFragment fragment = (MainActivityFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_place);
+                    fragment.addCity(mEdit.getText().toString());
+                    mEdit.setText("");
                 }
-                //  myString =  mEdit.getText().toString();
-                //  MainActivityFragment fragment = (MainActivityFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_place);
-                //  fragment.addCity(myString);
             }
         });
+        Log.w("activity","oncreate4");
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        Log.w("activity","option1");
         return true;
     }
 
@@ -51,6 +68,7 @@ public class MainActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+        Log.w("activity","option2");
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
--- app/src/main/java/com/example/mac/city/MainActivityFragment.java
+++ app/src/main/java/com/example/mac/city/MainActivityFragment.java
@@ -3,11 +3,17 @@ package com.example.mac.city;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import android.widget.AdapterView.AdapterContextMenuInfo;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -20,46 +26,81 @@ public class MainActivityFragment extends Fragment {
 
     public MainActivityFragment() {
     }
-    private void addCity(String city){
-        cityList.add(city);
-    }
-    private ArrayList<String> cityList;
-    private ArrayAdapter<String> mCityAdapter;
 
+    private ArrayAdapter<String> mCityAdapter;
+    private List<String> list;
+    private String[] cList = { };
+
+    public void addCity(String city){
+        list.add(city);
+        mCityAdapter.notifyDataSetChanged();
+        Log.w("fragment","addcity");
+    }
+
+    private void removeCity(Integer id) {
+        Log.w("fragment",list.get(id));
+        list.remove(id);
+        mCityAdapter.notifyDataSetChanged();
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        Log.w("fragment","oncreate1");
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
         String[] cities = {};
-        String value="";
-        cityList = new ArrayList<String>(Arrays.asList(cities));
+        ArrayList<String> cityList = new ArrayList<String>(Arrays.asList(cities));
 
-        Bundle extras = getActivity().getIntent().getExtras();
-        if (extras != null) {
-            Log.w("1111", "zaaa");
-            if(extras.getString("key") != null){
-                Log.w("2222", extras.getString("key"));
-                value = extras.getString("key");
-                //The key argument here must match that used in the other activity
-                if (value != null){
-                    Log.w("3333", value);
-                    cityList.add(value);
-                }
-            }
-        }
-
-
-        List<String> list = cityList;
+        list = cityList;
         mCityAdapter = new ArrayAdapter<String>(
                 getActivity(),
                 R.layout.list_item,
                 R.id.list_item_city_textview,
                 list
         );
-        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-
-        ListView listview = (ListView) rootView.findViewById(R.id.city_list);
+        Log.w("fragment","oncreate2");
+        final ListView listview = (ListView) rootView.findViewById(R.id.city_list);
         listview.setAdapter(mCityAdapter);
+        registerForContextMenu(listview);
 
+        Log.w("fragment","oncreate3");
+
+/*        listview.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> arg0, View arg1,
+                                       int pos, long id) {
+                listview.showContextMenu();
+                return true;
+            } });
+*/
         return rootView;
+    }
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo)menuInfo;
+        MenuInflater inflater = getActivity().getMenuInflater();
+       // menu.setHeaderTitle(list.get(info.position));
+       // Log.w("rosssss",list.get(info.position));
+        inflater.inflate(R.menu.context_menu, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        Log.w("xdxdxd","zaaa");
+        AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
+        switch (item.getItemId()) {
+            case R.id.action_delete:
+                Log.w("htgdkjygdlk","sddsssdsd");
+                removeCity(info.position);
+                Log.w("143141",Integer.toString(info.position));
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
     }
 }
